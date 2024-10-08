269c87635a4e24a41276a352a5837c76e9ee2289
jmiko4 <mik19003@byui.edu>
2024-10-08 14:05:21 -0600

    created tflite file

---
+++ convert-to-lite.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+# Load the trained model
+model = tf.keras.models.load_model('cat_classifier.h5')
+
+# Convert the model to TensorFlow Lite format
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Save the TensorFlow Lite model
+with open('cat_classifier.tflite', 'wb') as f:
+    f.write(tflite_model)
